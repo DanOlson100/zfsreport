b056d590b0fa35c8f202310ba8d782a7eda9830a
Dan Olson <danolson100@gmail.com>
2022-04-16 15:52:34 -0500

    Initial commit of the zfsreport.pl script with psuedo code.

---
+++ zfsreport.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+
+# Get The ZPool Status
+# Items to Collect
+#  - Name
+#  - Status
+#  - Read Errors
+#  - Write Errors
+#  - Checksum Error
+#  - % Used
+#  - Scrub Repaied Bytes
+#  - Scrub Errors
+#  - Last Scrub Age
+#  - Last Scrub Time
+#
+#  HDD SMART Report Summary
+#  Items to Collect
+#  - Device
+#  - Serial Number
+#  - SMART Status
+#  - Temp
+#  - Power-On Time
+#  - Start/Stop Count
+#  - Spin Retry Count
+#  - Realloced Sectors
+#  - Realloced Events
+#  - Current Pending Sectors
+#  - Offline Uncorrectable Sectors
+#  - UltraDMA CRC Errors
+#  - Seek Error Health
+#  - Last Test Age (Days)
+#  - Last Test Type
+
+# Print Storage Struct
+# Send Email
+# Get Smart Data
+# Get ZFS Pool Data
+#
